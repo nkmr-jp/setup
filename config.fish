193e5f8293015aea64ef7143600c9522e8255279
nkmr-jp <nkmr.git@gmail.com>
2020-09-07 16:45:45 +0900

    Update config.sh

--- config.fish
+++ config.fish
@@ -1,17 +1,30 @@
 # alias
 alias l='ls'
 alias lla='ll -a'
-alias op 'open'
-alias rm 'rmtrash'
-alias pecob 'peco --layout bottom-up'
-alias chrome "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
-alias load 'exec $SHELL -l'
-alias c 'clear'
-alias p 'pwd'
-alias gmoji 'gitmoji'
-alias g 'hub'
-alias m 'code ~/ghq/github.com/nkmr-jp/setup/.messages'
-alias land 'goland'
+alias op='open'
+alias rm='rmtrash'
+alias pecob='peco --layout bottom-up'
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+alias load='exec $SHELL -l'
+alias c='clear'
+alias p='pwd'
+alias gmoji='gitmoji'
+alias g='hub'
+alias m='code ~/ghq/github.com/nkmr-jp/setup/.messages'
+alias land='goland'
+alias config='code /Users/nkmr/ghq/github.com/nkmr-jp/setup'
+
+# open
+alias help-go-mod='open https://github.com/golang/go/wiki/Modules'
+function s-go
+    open "https://golang.org/search?q=$argv"
+end
+function s-repo
+    open "https://github.com/nkmr-jp?tab=repositories&q=$argv"
+end
+function s-github
+    open "https://github.com/search?q=$argv"
+end
 
 # fish settings
 

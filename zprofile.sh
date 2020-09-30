0d276e12c1cb0a807d06eeb97e532b2de85d1ab0
nkmr-jp <nkmr.git@gmail.com>
2020-09-30 15:24:10 +0900

    up

--- zprofile.sh
+++ zprofile.sh
@@ -4,9 +4,8 @@ eval "$(anyenv init - zsh)"
 # path
 export PATH="$GOPATH/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
-export PATH="$HOME/ghq/github.com/nkmr-jp/setup/bin:$PATH"
-
-chmod a+x $HOME/ghq/github.com/nkmr-jp/setup/bin/*
+# export PATH="$HOME/ghq/github.com/nkmr-jp/setup/bin:$PATH"
+# chmod a+x $HOME/ghq/github.com/nkmr-jp/setup/bin/*
 
 # save path order
 export ZSH_PATH=$PATH

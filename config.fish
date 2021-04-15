23e4417e526ba1c8153fa83fb9d8b6b3f54fea9b
nkmr-jp <nkmr.git@gmail.com>
2021-04-15 12:03:28 +0900

    up

--- config.fish
+++ config.fish
@@ -2,7 +2,7 @@
 alias l='ls'
 alias lla='ll -a'
 alias op='open'
-alias rm='rmtrash'
+alias rm='trash-put'
 alias pecob='peco --layout bottom-up'
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 alias load='exec $SHELL -l'

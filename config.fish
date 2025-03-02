4ae70bba776b504d203282ca02256f0f60d636e2
nkmr-jp <nkmr-jp@users.noreply.github.com>
2025-03-03 02:07:03 +0900

    add wind

--- config.fish
+++ config.fish
@@ -24,6 +24,7 @@ alias opf='open https://console.cloud.google.com/functions/list'
 alias opb='open https://console.cloud.google.com/bigquery'
 alias opr='open https://console.cloud.google.com/run'
 alias ope='open https://console.cloud.google.com/eventarc/triggers'
+alias wind='windsurf'
 
 
 # util
